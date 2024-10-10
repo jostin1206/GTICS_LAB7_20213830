cbfbd1cd41ba4818548f5c67722d14a6306e123f
jostin1206 <a20213830@pucp.edu.pe>
2024-10-09 19:24:34 -0500

    lab

---
+++ laboratorio7/src/main/java/com/example/laboratorio7/Laboratorio7Application.java
@@ -0,0 +1,13 @@
+package com.example.laboratorio7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Laboratorio7Application.class, args);
+    }
+
+}
